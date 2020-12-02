ec3ec4fbef31177c414e50c0942f3cf04842b871
Liam Isaacs <61097920+yi-ye-zhi-qiu@users.noreply.github.com>
2020-12-02 09:00:57 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@
 /*jshint browser:true */
 /*global XLSX */
 /* use webpack, need build tool for JS in order to use require for client-side code*/
+/* need to use webpack to load FileSaver, JSZip and run client-side without script tags in html */
 
 var X = typeof require !== "undefined" && require('../node_modules/xlsx') || XLSX;
+
+var JSZip = require('../node_modules/jszip/dist/jszip.min.js')
+var FileSaver = require('../node_modules/file-saver/dist/FileSaver.js')
 
 var global_wb;
 
 var process_wb = (function() {
+	
 	var OUT = document.getElementById('out');
 	var HTMLOUT = document.getElementById('htmlout');
 
@@ -28,25 +33,38 @@ var process_wb = (function() {
 	};
 
 	var to_json_linebyline = function to_json_linebyline(wb){
-		var sheet = wb.Sheets['Sheet1'];
-		var result = {};
-		var row, rowNum, colNum;
-		var range = X.utils.decode_range(sheet['!ref']);
-		for(rowNum = range.s.r; rowNum <= range.e.r-2; rowNum++){
-			row = [];
-			for(colNum=range.s.c; colNum<=range.e.c; colNum++){
-			   var nextCell = sheet[
-			      X.utils.encode_cell({r: rowNum, c: colNum})
-			   ];
-			   if( typeof nextCell === 'undefined' ){
-			      row.push(void 0);
-			   } else row.push(nextCell.w);
-			}
-			result[nextCell.v] = row;
-		    }
-		    return JSON.stringify(result, 2, 2);
-		}
+	    //NOTICE: must be named Sheet1...will find a way around that soon enough.
+	    var zip = new JSZip();
+	    var sheet = wb.Sheets['Sheet1'];
+	    var results = [];
+	    var range = X.utils.decode_range(sheet['!ref']);
+	    for(let rowNum = (range.s.r+1); rowNum <= range.e.r--; rowNum++){
+	       let thisRow = {},
+	           thisNode = '';
+	       for(let colNum=range.s.c; colNum<=range.e.c; colNum++){
+	          var thisHeader = sheet[X.utils.encode_cell({r: 0, c: colNum})].w
+	          var thisCell = sheet[X.utils.encode_cell({r: rowNum, c: colNum})].w
+	          if(colNum === 0){
+	            thisNode = thisCell;
+	          }
+	          thisRow[thisHeader] = thisCell;
+	       }
+	       thisResult = {};
+	       thisResult[thisNode] = [thisRow]
+	       results.push(thisResult)
 
+	       if(rowNum === 1){
+		   console.log('The point of this program is to return a zip folder full of JSON-ified excel data rows, as an example, the JSON of row ' + rowNum + ' will show as: \n' + JSON.stringify(thisResult, 2, 2));
+	       }
+		   zip.file(''.concat(rowNum) + '.json', JSON.stringify(thisResult, 2, 2))
+	       }
+
+		zip.generateAsync({type: "blob"})
+		  .then(function(content) {
+			saveAs(content, "result_" + new Date() + ".zip");
+		   });
+		return 'full unsplit JSON: \n' + JSON.stringify(results, 2, 2)
+	}
 
 	return function process_wb(wb) {
 		global_wb = wb;
@@ -63,15 +81,6 @@ var process_wb = (function() {
 		if(OUT.innerText === undefined) OUT.textContent = output;
 		else OUT.innerText = output;
 		if(typeof console !== 'undefined') console.log("output ", new Date());
-
-		/*var datastr="data:text/json;charset=utf-8," + encodeURIComponent(output);
-		var exportName = 'hello'
-		var downloadAnchorNode = document.createElement('a');
-		downloadAnchorNode.setAttribute("href",     datastr);
-		downloadAnchorNode.setAttribute("download", exportName + ".json");
-		document.body.appendChild(downloadAnchorNode); // required for firefox
-		downloadAnchorNode.click();
-		downloadAnchorNode.remove();*/
 	}
 
 })();
